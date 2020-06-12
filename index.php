6d3261bd3354087e3af341754ffcb7ce570e4d94
EdwardASNL17 <edwardasnl@mail.ru>
2020-06-12 18:51:35 +0300

    Парсится ссылка, добавляется в слайдер

--- index.php
+++ index.php
@@ -27,6 +27,8 @@ if (isset($_POST['serial'])) {
 
 	}
 	$photo = $photo[$size-1];
+	$src = $photo->src;
+
 
 }
 
@@ -68,6 +70,26 @@ if (isset($_POST['serial'])) {
  </div>
  </nav>
  </header>
+  <div class="container-fluid p-0">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="<?php echo $src;?>" alt="First slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </div>
  <div class="container" style="margin-top: 70px;">
  	<p><?php foreach ($serial as $key => $value) {
 	echo $key." ".$value;
@@ -79,8 +101,9 @@ if (isset($_POST['serial'])) {
 	echo "<br><br>";
 	}  ?>
 	</p>
-	<pre><?php echo "$photo"; ?></pre>
  </div>
- <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </body>
 </html>
